963f2d1255f9bb174461aff073beb59a44fb194d
t o <finalfantasytimothy@gmail.com>
2021-12-28 22:13:20 -0700

    Added missing pragma in Math.h. Added Camera.h and some basic initalization details.

---
+++ src/engine/Camera.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "Math.h"
+
+// This is what I have in my Fatal Frame 1 Ghidra project - weirdbeard
+typedef struct cam_wrk
+{
+	Vector4 position;
+	Vector4 rotation;
+	float unknown1;
+	float unknown2;
+};
+
+// Since this is C++ bois
+class Camera
+{
+private:
+	cam_wrk cam;
+
+public:
+	// The functions that turn Finder mode on and off in Fatal Frame 1. Does this match Fatal Frame 2 and 3?
+	void FinderInSet();
+	void FinderOutSet();
+
+	// Camera position?
+	Vector4 GetCamI2DPos();
+
+};
--- src/engine/Math.h
+++ src/engine/Math.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 
 typedef struct Vector3
