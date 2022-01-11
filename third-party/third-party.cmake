aac377183fe0572f1f0a0002273e591cddd161eb
William Adam-Grenier <william.a-g@hotmail.com>
2022-01-10 20:56:05 -0500

    Added Imgui
    * Added Imgui 
    * Backends required to be compiled directly with the exe

--- third-party/third-party.cmake
+++ third-party/third-party.cmake
@@ -7,4 +7,13 @@ endif()
 
 find_package(OpenGL)
 
-set(THIRD_PARTY_DEPS glfw ${OPENGL_LIBRARIES} ${GLFW_LIBRARIES})
+set(IMGUI_PATH third-party/imgui)
+file(GLOB IMGUI_SOURCES ${IMGUI_PATH}/*.cpp)
+add_library("ImGui" STATIC ${IMGUI_SOURCES})
+target_include_directories("ImGui" PUBLIC ${IMGUI_PATH})
+
+set(IMGUI_BACKEND_SOURCES ${IMGUI_PATH}/backends/imgui_impl_glfw.cpp ${IMGUI_PATH}/backends/imgui_impl_opengl3.cpp)
+set(IMGUI_BACKEND_HEADERS ${IMGUI_PATH}/backends/imgui_impl_glfw.h ${IMGUI_PATH}/backends/imgui_impl_opengl3.h ${IMGUI_PATH}/backends/imgui_impl_opengl3_loader.h)
+
+
+set(THIRD_PARTY_DEPS glfw ${OPENGL_LIBRARIES} ${GLFW_STATIC_LIBRARIES} ${GLFW_LIBRARIES} ImGui)
