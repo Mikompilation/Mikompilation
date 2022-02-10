03cc6a641fae17c094c1d812c98cf1b155290d2a
t o <finalfantasytimothy@gmail.com>
2022-02-10 09:43:30 -0700

    Moved to ui folder. Added reveresed main function

--- src/main.cpp
+++ src/main.cpp
@@ -7,3 +7,34 @@ int main(int argc, char *argv[])
   window.Update();
   return 0;
 }
+
+// ToDo rename to main() after things have been moved around abit
+int Gmain()
+{
+  bool isRunning;
+  bool fmvPlayed;
+	//InitSystems();
+
+  while (!isRunning)
+  {
+	  // I wOnDer hOw lOnG itS bEeN. Since my brother and i... Began to see things. Other people can't see. *Fatal Frame noises*
+    //fmvPlayed = playFmv(0);
+      while (fmvPlayed)
+      {
+        // ToDo call all main and init functions in here
+          // GameMain();
+          // DrawGS(); // Would be called sceGsSyncPath();
+          // DrawAll2DMes_P2();
+          // FlushModels(1);
+          // ClearTextureCache();
+          // SeCtrlMain();
+        //if (SoftReset())
+        //{
+        // We back in bios?
+        //}
+      }
+  }
+
+  return 0;
+      
+}
