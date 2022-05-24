5e1fbd150b69c22e7a0630a2d48d358cec8eb9b4
t o <finalfantasytimothy@gmail.com>
2022-05-24 16:04:01 -0700

    Added inital bst nodes

--- engine/memory/heap.h
+++ engine/memory/heap.h
@@ -1,11 +1,19 @@
 #include <iostream>
+#include "memory.h"
 
 template<class T>
 class Heap
 {
  private:
-  T *heap;
+  T *root;
+  T *left;
+  T *right;
+
 
  public:
+  void push(T data);
+  void pop(T data);
+  // Make Heap?
+  T heapCrlsMalloc(Memory *param1, int param2);
   T my_mallocMalloc();
 };
