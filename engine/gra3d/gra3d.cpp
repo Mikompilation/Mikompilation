ebc311c252ce86a78a2d66a28d3f72f4ac672591
kenshen112 <finalfantasytimothy@gmail.com>
2022-06-04 18:32:50 -0700

    Added CoreObject ScratchPadLayout and ScratchPadLayout_MapShadow. Start of ScratchPad implementation

--- engine/gra3d/gra3d.cpp
+++ engine/gra3d/gra3d.cpp
@@ -5,3 +5,11 @@ void gra3dInit()
 {
   printNotImplemented(GAME_LOGGER, __FUNCTION__, __FILE__);
 }
+
+
+// Need's to return from memory... Potentially ScratchPad memory? An instance of the requested obejct like CoreObject or ScratchPadObject
+template <class T>
+T* GetStaticInstance()
+{
+  return this; // ToDo, replace this with pointer cast!
+}
--- engine/gra3d/gra3d.h
+++ engine/gra3d/gra3d.h
@@ -1,3 +1,24 @@
 #pragma once
 
+struct GRA3DCOREOBJECT
+{
+
+};
+
+struct GRA3DSCRATCHPADLAYOUT
+{
+
+};
+
+struct GRA3DSCRATCHPADLAYOUT_MAPSHADOW
+{
+
+};
+
+static GRA3DCOREOBJECT *core;
+static GRA3DSCRATCHPADLAYOUT *scratchpad;
+
+template <class T>
+T* GetStaticInstance();
+
 void gra3dInit();
