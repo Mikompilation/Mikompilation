ff7d233e0cce1edadc6747036bf884c38d82b80f
kenshen112 <finalfantasytimothy@gmail.com>
2022-08-14 12:13:56 -0700

    Added INGAME_WRK
    Added INGAME_WRK class, functions and data vars within

--- game/ingame.cpp
+++ game/ingame.cpp
@@ -1,7 +1,48 @@
 #include "ingame.h"
 #include "logging/printing.h"
 
-void IngameWrkInit(int param1, int param2)
+void INGAME_WRK::Init()
 {
-  printNotImplemented(GAME_LOGGER, __FUNCTION__, __FILE__);
+  MenuLockCnt = 0;
+  PauseLockCnt = 0;
+}
+
+void INGAME_WRK::MenuLock()
+{
+  MenuLockCnt += 1;
+}
+
+void INGAME_WRK::MenuUnlock()
+{
+  MenuLockCnt -= 1;
+
+  if (MenuLockCnt < 0)
+  {
+    printError(GAME_LOGGER, "MenuUnlock", "MenuLock null or negative");
+  }
+}
+
+bool INGAME_WRK::MenuIsLocked()
+{
+  return MenuLockCnt > 0;
+}
+
+void INGAME_WRK::PauseLock()
+{
+  PauseLockCnt += 1;
+}
+
+void INGAME_WRK::PauseUnlock()
+{
+  PauseLockCnt -= 1;
+
+  if (PauseLockCnt < 0)
+  {
+    printError(GAME_LOGGER, "PauseUnlock", "PauseLock null or negative");
+  }
+}
+
+bool INGAME_WRK::PauseIsLocked()
+{
+  return PauseLockCnt > 0;
 }
--- game/ingame.h
+++ game/ingame.h
@@ -1,3 +1,20 @@
 #pragma once
+#include <stdint.h>
 
-void IngameWrkInit(int gPhase1, int gPhase2);
+struct INGAME_WRK
+{
+  uint8_t ChapterNo;
+  uint8_t Difficulty;
+  uint8_t ClearCnt;
+  uint8_t ClearSaveFlag;
+  int MenuLockCnt;
+  int PauseLockCnt;
+
+  void Init();
+  void MenuLock();
+  void MenuUnlock();
+  bool MenuIsLocked();
+  void PauseLock();
+  void PauseUnlock();
+  bool PauseIsLocked();
+};
